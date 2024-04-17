c063c8e181a2711da02ba51eb64b7d4999763eda
ariana <ariana.nurlayla@ui.ac.id>
2024-04-17 19:25:47 +0700

    Enable external access to profile feature endpoints

--- src/main/java/id/ac/ui/cs/pustakaone/identity/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/controller/HomeController.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.pustakaone.identity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -10,4 +11,13 @@ public class HomeController {
         return "Hello identity!";
     }
 
+    @GetMapping("/profile")
+    public String getRetrieveProfile() {
+        return "Hello retrieve profile!";
+    }
+
+    @GetMapping("/profile/{id}")
+    public String patchUpdateProfile(@PathVariable int id) {
+        return "Hello update profile for id: " + id;
+    }
 }
