eb73fb67d05f51f953935b859b264be583a1cadf
ariana <ariana.nurlayla@ui.ac.id>
2024-04-24 19:22:04 +0700

    [GREEN] Implement User model

--- src/main/java/id/ac/ui/cs/pustakaone/identity/model/User.java
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/model/User.java
@@ -4,15 +4,16 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
 
-import lombok.*;
-
-import lombok.*;
-//
-//import javax.persistence.*;
-//import javax.validation.constraints.NotBlank;
-//import javax.validation.constraints.Size;
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @Setter
@@ -20,34 +21,40 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "email"),
-        @UniqueConstraint(columnNames = "no_telp"),
-})
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
     @Id
+    @Column(name = "u_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
-    @Size(max = 128)
-    private String nama;
+    @Column(name = "username", unique = true)
+    private String username;
 
     @NotBlank
-    @Size(max = 256)
-    @Column(unique = true)
+    @Column(name = "password")
+    private String password;
+
+    @NotBlank
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "is_admin")
+    private Boolean isAdmin;
+
+    @Column(name = "email", unique = true)
     private String email;
 
     @NotBlank
-    @Size(max = 64)
     @Column(name = "no_telp", unique = true)
     private String noTelp;
 
-    @NotBlank
-    @Size(max = 128)
-    private String password;
-
     @Lob
+    @Column(name = "foto")
     private byte[] foto;
 
     @Column(name = "jenis_kelamin")
@@ -56,13 +63,35 @@ public class User {
     @Column(name = "tanggal_lahir")
     private LocalDate tanggalLahir;
 
-    private String bio;
-
-    public User(String nama, String email, String noTelp, String password) {
-        this.nama = nama;
+    public User(String username, String email, String noTelp, String password) {
+        this.username = username;
         this.email = email;
         this.noTelp = noTelp;
         this.password = password;
     }
-}
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
