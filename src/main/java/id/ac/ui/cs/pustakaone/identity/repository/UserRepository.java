eb090d88d116c50ef5487f6cfbae7323277bdee4
ariana <ariana.nurlayla@ui.ac.id>
2024-04-24 19:34:23 +0700

    [GREEN] Implement find user by username

---
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.pustakaone.identity.repository;
+
+import id.ac.ui.cs.pustakaone.identity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+//    Optional<User> findById(Long id);
+}
+
