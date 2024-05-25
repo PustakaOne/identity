292ce589021fb0edd76a2fca85122e1d93f31eb8
ariananurlayla <ariananurlaylas@gmail.com>
2024-05-25 22:34:42 +0700

    [GREEN] implement repository

---
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/repository/TokenRepository.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.pustakaone.identity.repository;
+
+import id.ac.ui.cs.pustakaone.identity.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    @Query("""
+    select t from Token t inner join User u on t.user.id = u.id
+    where u.id = :userId and (t.expired = false or t.revoked = false)
+    """)
+    List<Token> findAllValidTokensByUser(String userId);
+    Optional<Token> findByTokenString(String tokenString);
+}
---
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.pustakaone.identity.repository;
+
+import id.ac.ui.cs.pustakaone.identity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+
+}
