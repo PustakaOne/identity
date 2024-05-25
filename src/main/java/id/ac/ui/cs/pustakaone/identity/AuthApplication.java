7f90bcc1f7c1e857f695f51d913cb76dde5d4897
ariananurlayla <ariananurlaylas@gmail.com>
2024-05-25 22:37:18 +0700

    [GREEN] implement AuthApplication

--- src/main/java/id/ac/ui/cs/pustakaone/identity/AuthApplication.java
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/AuthApplication.java
@@ -1,13 +1,34 @@
+//package id.ac.ui.cs.pustakaone.identity;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class AuthenticationApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(AuthenticationApplication.class, args);
+//	}
+//
+//}
 package id.ac.ui.cs.pustakaone.identity;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 
-@SpringBootApplication
-public class AuthenticationApplication {
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class,
+		ReactiveSecurityAutoConfiguration.class,
+		ReactiveManagementWebSecurityAutoConfiguration.class,
+}
+)
+public class AuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationApplication.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 
 }
