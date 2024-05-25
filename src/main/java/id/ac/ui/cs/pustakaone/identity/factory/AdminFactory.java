afa5f97234a9c18ab68b555b7d5f48759b2c0060
ariananurlayla <ariananurlaylas@gmail.com>
2024-05-25 22:31:29 +0700

    [GREEN] implement factory

---
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/factory/AdminFactory.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.pustakaone.identity.factory;
+
+import id.ac.ui.cs.pustakaone.identity.dto.RegisterRequest;
+import id.ac.ui.cs.pustakaone.identity.model.User;
+
+public class AdminFactory implements FactoryUser{
+    @Override
+    public User createUser(RegisterRequest request) {
+        return User.builder()
+                .fullName(request.getFullName())
+                .username(request.getUsername())
+                .active(true)
+                .email(request.getEmail())
+                .noTelp(request.getNoTelp())
+                .password(request.getPassword())
+                .foto(request.getFoto())
+                .jenisKelamin(request.getJenisKelamin())
+                .tanggalLahir(request.getTanggalLahir())
+                .bio(request.getBio())
+                .role("ADMIN")
+                .build();
+    }
+}
