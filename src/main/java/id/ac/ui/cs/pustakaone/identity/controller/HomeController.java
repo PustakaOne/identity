65806a7c6cdace2ffce7a700244a163120c76559
ariananurlayla <ariananurlaylas@gmail.com>
2024-05-25 22:28:10 +0700

    [RED] make test for controller

--- src/main/java/id/ac/ui/cs/pustakaone/identity/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/controller/HomeController.java
@@ -1,13 +1,13 @@
-package id.ac.ui.cs.pustakaone.identity.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController()
-public class HomeController {
-    @GetMapping("/")
-    public String getHello() {
-        return "Hello identity!";
-    }
-
-}
+//package id.ac.ui.cs.pustakaone.identity.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController()
+//public class HomeController {
+//    @GetMapping("/")
+//    public String getHello() {
+//        return "Hello identity!";
+//    }
+//
+//}
