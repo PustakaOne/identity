0151e09e70548721695dcf8b61ecce659b391dae
ariananurlayla <ariana.nurlayla@ui.ac.id>
2024-05-26 06:57:21 +0700

    [green] implement user service

---
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/service/UserService.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.pustakaone.identity.service;
+
+import id.ac.ui.cs.pustakaone.identity.model.User;
+import id.ac.ui.cs.pustakaone.identity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
