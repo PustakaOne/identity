cd2e2a5d0cc7de62571a2e89fcdd0be5a5d0f1e8
ariananurlayla <ariananurlaylas@gmail.com>
2024-05-25 22:22:52 +0700

    update dependencies

--- src/main/java/id/ac/ui/cs/pustakaone/identity/AuthApplication.java
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/AuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IdentityApplication {
+public class AuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IdentityApplication.class, args);
+		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
 }
