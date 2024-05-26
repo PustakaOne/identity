ed6ee1e1bba19413fc768281b0595a5a283386af
ariananurlayla <ariana.nurlayla@ui.ac.id>
2024-05-26 08:16:08 +0700

    [refactor] update User model to enable user retrieval

--- src/main/java/id/ac/ui/cs/pustakaone/identity/model/User.java
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/model/User.java
@@ -2,10 +2,7 @@ package id.ac.ui.cs.pustakaone.identity.model;
 
 import id.ac.ui.cs.pustakaone.identity.Enum.Role;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
