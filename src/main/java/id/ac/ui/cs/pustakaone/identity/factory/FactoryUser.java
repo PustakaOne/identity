4aa8642fcb950e9205d3a6efa5acde0e5216524e
RafliMahesa <muhammadraflimahesa@gmail.com>
2024-05-26 11:32:09 +0700

    [REFACTOR] remove factory folder

--- src/main/java/id/ac/ui/cs/pustakaone/identity/factory/FactoryUser.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.pustakaone.identity.factory;
-
-import id.ac.ui.cs.pustakaone.identity.dto.RegisterRequest;
-import id.ac.ui.cs.pustakaone.identity.model.User;
-
-public interface FactoryUser {
-    User createUser(RegisterRequest request);
-}
