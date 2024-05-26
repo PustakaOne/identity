aa18a25e5577fc8827f713cf1542dda5ee8cdee6
ariananurlayla <ariana.nurlayla@ui.ac.id>
2024-05-26 07:56:48 +0700

    [green] implement user service

--- src/main/java/id/ac/ui/cs/pustakaone/identity/service/UserService.java
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/service/UserService.java
@@ -22,7 +22,4 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
 }
