a93d752ad384fc8e9ea187322204342702e7154a
ariananurlayla <ariana.nurlayla@ui.ac.id>
2024-05-26 09:02:37 +0700

    [refactor] optimize import

--- src/main/java/id/ac/ui/cs/pustakaone/identity/controller/UserController.java
+++ src/main/java/id/ac/ui/cs/pustakaone/identity/controller/UserController.java
@@ -5,7 +5,6 @@ import id.ac.ui.cs.pustakaone.identity.model.User;
 import id.ac.ui.cs.pustakaone.identity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
